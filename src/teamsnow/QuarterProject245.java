62a03162a5b3a65f4e77dc4823ecd8f939f0816b
Daniel <dmfierros@cpp.edu>
2016-08-08 14:20:45 -0700

    added end game page
    panel goes back t main menu

--- src/teamsnow/QuarterProject245.java
+++ src/teamsnow/QuarterProject245.java
@@ -59,7 +59,6 @@ public class QuarterProject245 extends javax.swing.JFrame {
         jLabel8 = new javax.swing.JLabel();
         PlayMenu = new javax.swing.JPanel();
         backButton2 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         aButton = new javax.swing.JButton();
@@ -88,6 +87,11 @@ public class QuarterProject245 extends javax.swing.JFrame {
         qButton = new javax.swing.JButton();
         nButton = new javax.swing.JButton();
         rButton = new javax.swing.JButton();
+        jLabel11 = new javax.swing.JLabel();
+        End = new javax.swing.JPanel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        bTMButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAutoRequestFocus(false);
@@ -130,8 +134,8 @@ public class QuarterProject245 extends javax.swing.JFrame {
             .addGroup(SplashPageLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(ProjectTitle, javax.swing.GroupLayout.DEFAULT_SIZE, 157, Short.MAX_VALUE)
-                .addGap(78, 78, 78)
-                .addComponent(TeamName, javax.swing.GroupLayout.DEFAULT_SIZE, 58, Short.MAX_VALUE)
+                .addGap(79, 79, 79)
+                .addComponent(TeamName, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
                 .addGap(40, 40, 40)
                 .addComponent(debugButton)
                 .addGap(38, 38, 38))
@@ -322,17 +326,15 @@ public class QuarterProject245 extends javax.swing.JFrame {
             }
         });
 
-        jTextField1.setText("Date goes here");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
-
         jLabel9.setFont(new java.awt.Font("Dialog", 1, 36)); // NOI18N
         jLabel9.setText("HangMan");
 
         jButton1.setText("Skip");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         aButton.setText("A");
 
@@ -363,11 +365,6 @@ public class QuarterProject245 extends javax.swing.JFrame {
         sButton.setText("S");
 
         tButton.setText("T");
-        tButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                tButtonActionPerformed(evt);
-            }
-        });
 
         uButton.setText("U");
 
@@ -391,6 +388,8 @@ public class QuarterProject245 extends javax.swing.JFrame {
 
         rButton.setText("R");
 
+        jLabel11.setText("Date goes here");
+
         javax.swing.GroupLayout PlayMenuLayout = new javax.swing.GroupLayout(PlayMenu);
         PlayMenu.setLayout(PlayMenuLayout);
         PlayMenuLayout.setHorizontalGroup(
@@ -400,14 +399,11 @@ public class QuarterProject245 extends javax.swing.JFrame {
                 .addGroup(PlayMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PlayMenuLayout.createSequentialGroup()
                         .addComponent(jLabel9)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 335, Short.MAX_VALUE)
                         .addGroup(PlayMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PlayMenuLayout.createSequentialGroup()
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap())
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PlayMenuLayout.createSequentialGroup()
-                                .addComponent(jButton1)
-                                .addGap(29, 29, 29))))
+                            .addComponent(jButton1)
+                            .addComponent(jLabel11))
+                        .addGap(29, 29, 29))
                     .addGroup(PlayMenuLayout.createSequentialGroup()
                         .addGroup(PlayMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(backButton2)
@@ -472,11 +468,11 @@ public class QuarterProject245 extends javax.swing.JFrame {
         PlayMenuLayout.setVerticalGroup(
             PlayMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PlayMenuLayout.createSequentialGroup()
-                .addGap(18, 18, 18)
                 .addGroup(PlayMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(PlayMenuLayout.createSequentialGroup()
+                        .addGap(18, 18, 18)
                         .addComponent(jLabel9)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 239, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 242, Short.MAX_VALUE)
                         .addGroup(PlayMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(aButton)
                             .addComponent(bButton)
@@ -493,8 +489,9 @@ public class QuarterProject245 extends javax.swing.JFrame {
                             .addComponent(mButton))
                         .addGap(1, 1, 1))
                     .addGroup(PlayMenuLayout.createSequentialGroup()
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGap(12, 12, 12)
+                        .addComponent(jLabel11)
+                        .addGap(18, 18, 18)
                         .addComponent(jButton1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addGroup(PlayMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -517,6 +514,50 @@ public class QuarterProject245 extends javax.swing.JFrame {
         );
 
         mainPanel.add(PlayMenu, "playCard");
+
+        jLabel10.setFont(new java.awt.Font("Dialog", 1, 36)); // NOI18N
+        jLabel10.setText("Your score is ");
+
+        jLabel12.setText("score");
+
+        bTMButton.setText("End Hangman");
+        bTMButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bTMButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout EndLayout = new javax.swing.GroupLayout(End);
+        End.setLayout(EndLayout);
+        EndLayout.setHorizontalGroup(
+            EndLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(EndLayout.createSequentialGroup()
+                .addContainerGap(201, Short.MAX_VALUE)
+                .addGroup(EndLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, EndLayout.createSequentialGroup()
+                        .addComponent(jLabel10)
+                        .addGap(183, 183, 183))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, EndLayout.createSequentialGroup()
+                        .addComponent(jLabel12)
+                        .addGap(300, 300, 300))))
+            .addGroup(EndLayout.createSequentialGroup()
+                .addGap(257, 257, 257)
+                .addComponent(bTMButton)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        EndLayout.setVerticalGroup(
+            EndLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(EndLayout.createSequentialGroup()
+                .addGap(84, 84, 84)
+                .addComponent(jLabel10)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel12)
+                .addGap(108, 108, 108)
+                .addComponent(bTMButton)
+                .addContainerGap(111, Short.MAX_VALUE))
+        );
+
+        mainPanel.add(End, "card7");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -579,13 +620,17 @@ public class QuarterProject245 extends javax.swing.JFrame {
       card.show(mainPanel, "mainmenuCard");
    }//GEN-LAST:event_backButton2ActionPerformed
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-       System.out.println("hello");
-    }//GEN-LAST:event_jTextField1ActionPerformed
-
-    private void tButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tButtonActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_tButtonActionPerformed
+      CardLayout card = (CardLayout)mainPanel.getLayout();
+      card.show(mainPanel, "mainmenuCard");
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void bTMButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bTMButtonActionPerformed
+        // TODO add your handling code here:
+         CardLayout card = (CardLayout)mainPanel.getLayout();
+      card.show(mainPanel, "mainmenuCard");
+    }//GEN-LAST:event_bTMButtonActionPerformed
    
     /**
      * @param args the command line arguments
@@ -629,6 +674,7 @@ public class QuarterProject245 extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton CreditsButton;
     private javax.swing.JPanel CreditsMenu;
+    private javax.swing.JPanel End;
     private javax.swing.JButton HighScoresButton;
     private javax.swing.JPanel HighScoresMenu;
     private javax.swing.JButton LButton;
@@ -641,6 +687,7 @@ public class QuarterProject245 extends javax.swing.JFrame {
     private javax.swing.JLabel TeamName;
     private javax.swing.JButton aButton;
     private javax.swing.JButton bButton;
+    private javax.swing.JButton bTMButton;
     private javax.swing.JButton backButton0;
     private javax.swing.JButton backButton1;
     private javax.swing.JButton backButton2;
@@ -655,6 +702,9 @@ public class QuarterProject245 extends javax.swing.JFrame {
     private javax.swing.JButton jButton;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -663,7 +713,6 @@ public class QuarterProject245 extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JButton kButton;
     private javax.swing.JButton mButton;
     private javax.swing.JPanel mainPanel;
