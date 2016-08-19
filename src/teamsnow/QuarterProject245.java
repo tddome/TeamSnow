939975bb2679f9d3c9d9e68ba0578d083e52b527
tddome <troydome@yahoo.com>
2016-08-19 14:26:00 -0700

    Added Sudoku Board
    Now to get each square its own area, so it may be updated and compared
    with an answer key in the future

--- src/teamsnow/QuarterProject245.java
+++ src/teamsnow/QuarterProject245.java
@@ -154,6 +154,7 @@ public class QuarterProject245 extends javax.swing.JFrame {
       sudokuClock = new javax.swing.JLabel();
       sudokuSubmit = new javax.swing.JButton();
       sudokuQuit = new javax.swing.JButton();
+      jLabel6 = new javax.swing.JLabel();
 
       setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
       setAutoRequestFocus(false);
@@ -1211,33 +1212,50 @@ public class QuarterProject245 extends javax.swing.JFrame {
          }
       });
 
+      jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pictures/grid.jpg"))); // NOI18N
+      jLabel6.setText("board");
+      jLabel6.setMaximumSize(new java.awt.Dimension(359, 359));
+      jLabel6.setMinimumSize(new java.awt.Dimension(359, 359));
+      jLabel6.setPreferredSize(new java.awt.Dimension(359, 359));
+
       javax.swing.GroupLayout SudokuGameLayout = new javax.swing.GroupLayout(SudokuGame);
       SudokuGame.setLayout(SudokuGameLayout);
       SudokuGameLayout.setHorizontalGroup(
          SudokuGameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
          .addGroup(SudokuGameLayout.createSequentialGroup()
             .addContainerGap()
-            .addGroup(SudokuGameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-               .addComponent(SudokuTitle)
-               .addComponent(sudokuSubmit))
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 446, Short.MAX_VALUE)
-            .addGroup(SudokuGameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-               .addComponent(sudokuClock, javax.swing.GroupLayout.Alignment.TRAILING)
-               .addComponent(sudokuQuit, javax.swing.GroupLayout.Alignment.TRAILING))
+            .addComponent(SudokuTitle)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(sudokuClock)
             .addContainerGap())
+         .addGroup(SudokuGameLayout.createSequentialGroup()
+            .addGap(38, 38, 38)
+            .addComponent(sudokuSubmit)
+            .addGap(18, 18, 18)
+            .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGap(18, 18, 18)
+            .addComponent(sudokuQuit)
+            .addGap(0, 44, Short.MAX_VALUE))
       );
       SudokuGameLayout.setVerticalGroup(
          SudokuGameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
          .addGroup(SudokuGameLayout.createSequentialGroup()
             .addContainerGap()
-            .addGroup(SudokuGameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-               .addComponent(sudokuClock)
-               .addComponent(SudokuTitle))
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 276, Short.MAX_VALUE)
-            .addGroup(SudokuGameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-               .addComponent(sudokuSubmit)
-               .addComponent(sudokuQuit))
-            .addGap(54, 54, 54))
+            .addGroup(SudokuGameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+               .addGroup(javax.swing.GroupLayout.Alignment.LEADING, SudokuGameLayout.createSequentialGroup()
+                  .addGap(22, 22, 22)
+                  .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                  .addGap(0, 13, Short.MAX_VALUE))
+               .addGroup(javax.swing.GroupLayout.Alignment.LEADING, SudokuGameLayout.createSequentialGroup()
+                  .addComponent(SudokuTitle)
+                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                  .addComponent(sudokuSubmit)
+                  .addGap(59, 59, 59))
+               .addGroup(SudokuGameLayout.createSequentialGroup()
+                  .addComponent(sudokuClock)
+                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                  .addComponent(sudokuQuit)
+                  .addGap(56, 56, 56))))
       );
 
       mainPanel.add(SudokuGame, "sudokuCard");
@@ -3090,6 +3108,7 @@ public class QuarterProject245 extends javax.swing.JFrame {
    private javax.swing.JLabel jLabel3;
    private javax.swing.JLabel jLabel4;
    private javax.swing.JLabel jLabel5;
+   private javax.swing.JLabel jLabel6;
    private javax.swing.JLabel jLabel9;
    private javax.swing.JLabel jLabelColor;
    private javax.swing.JLabel jLabelHangman;
